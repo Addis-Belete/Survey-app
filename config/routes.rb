3cef4ba10b25d82bcffbbfe48040a87d278fcfa8
Addis Belete <addis.bel@gmail.com>
2021-12-20 16:38:16 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
+  shallow do
+    resources :users do
+      resources :questions, :answers
+    end
+  end
 end

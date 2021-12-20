a969b0f783eafe645a08c07a98635c93bb919b22
Addis Belete <addis.bel@gmail.com>
2021-12-20 17:05:18 +0300

    Add a user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def index
+    json_response(@user.all)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    json_response(@user)
+  end
+
+  def create
+    @user = User.create!(user_params)
+    json_response(@user, :created)
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password, :confirm_password)
+  end
 end

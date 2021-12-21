c3d61111cc1cab1ca5e4877bd72dd4bd3065424e
Addis Belete <addis.bel@gmail.com>
2021-12-21 21:07:52 +0300

    Add a answer controller methods

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,15 @@
 class AnswersController < ApplicationController
+  def index
+    json_response(@answer.all)
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+    json_response(@answer)
+  end
+
+  def create
+    @answer = Answer.create!(question_params)
+    json_response(@answer, :created)
+  end
 end

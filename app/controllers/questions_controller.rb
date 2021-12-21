f69765d4dae01e981942b56e6a61cc36785d9db2
Addis Belete <addis.bel@gmail.com>
2021-12-21 21:05:04 +0300

    Add question controller methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,15 @@
 class QuestionsController < ApplicationController
+  def index
+    json_response(@question.all)
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    json_response(@question)
+  end
+
+  def create
+    @question = Question.create!(question_params)
+    json_response(@user, :created)
+  end
 end

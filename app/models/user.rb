93413a306a2d8cc01bc87cb94ea7698554aa2b92
Addis Belete <addis.bel@gmail.com>
2021-12-22 10:34:07 +0300

    Add active record assocations between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, :password, :confirm_password, presence: true, length: { minimum: 5 }
+  has_many :question
+  has_many :answer
 end

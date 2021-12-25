b441ebc890b517dcd84acf177e22665a2b06880d
Addis Belete <addis.bel@gmail.com>
2021-12-25 11:25:51 +0300

    Add assocationa

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, :password, :confirm_password, presence: true, length: { minimum: 5 }
-  has_many :question
-  has_many :answer
+  has_many :survey
 end

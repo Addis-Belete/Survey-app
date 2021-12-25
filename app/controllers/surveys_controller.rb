8b0ac2284d66ee1cc8de71fe3aa2e9eb10a2d4e2
Addis Belete <addis.bel@gmail.com>
2021-12-25 12:31:17 +0300

    Add ceate and show method to Survey controller

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,16 @@
+class SurveysController < ApplicationController
+  def index
+    json_response(@survey.all)
+  end
+
+  def show
+    @survey = Survey.find(params[:id])
+    json_response(@survey)
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @survey = @user.surveys.create(survey_params)
+    json_response(@survey, :created)
+  end
+end

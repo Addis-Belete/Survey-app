f8a909ec3ac8f7602c1e174d519c6fd827008439
Addis Belete <addis.bel@gmail.com>
2021-12-31 15:23:16 +0300

    Add a route for login and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "users#index"
+  post "/login", to: "sessions#login"
+  post "/signup", to: "sessions#signup"
   resources :users do
     resources :surveys, only: [:new, :create, :update, :destroy] do
       resources :questions, only: [:new, :create, :update, :destroy]
